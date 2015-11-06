5a27990a38d665bc9068bd91c5740e1b1783986f
Chancer <Chancergs@hotmail.com>
2015-11-05 18:34:34 -0700

    Rough Draft Upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSEthics
-Final Project for CSCI_215.
+
+
+This is the github for my program for the final in CSCI_215. A lot of the files in here are just netbeans stuff.
+The actual .java files are under the src/csethics folder.
+
+
---
+++ src/csethics/CSEthics.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csethics;
+
+/**
+ *
+ * @author Chance Ronning Glenn
+ */
+
+    //Game Notes:
+
+    //Game will start on Day 1. Every Day you will be able to do a certain amount of things.
+    //Once you have decided which things to do, you press a "Run" button.
+    //You'll get income from your produced malware, along with a risk check that has a chance to proc negative events.
+    //Currently, the goal of the game will be to reach a certain amount of money by a certain time, maybe to pay off outrageous student loans or something.
+    //Different types of malware will have different risk increase amounts, and provide different amounts of money.
+    //A part of the GUI near the malware selection will be an information button. Upon clicked, it will tell you a little about the type of malware, and famous incidents involving the type.
+    //There will probably also be special actions you can take when you have certain amounts of things, such as DDoS attacks using trojan created zombies.
+
+
+public class CSEthics {
+
+    
+    public static void main(String[] args) {
+        //game launch thing will go here, need the gui for that though.
+        
+
+    }
+    
+}
